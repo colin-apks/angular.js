0a7fdc36b45cee2c1edcc0f6129845ba8e0b9ae5
colin <zjgulin2011@163.com>
2014-12-16 17:52:26 +0800

    http缓存修改

--- http/controllers.js
+++ http/controllers.js
@@ -1,7 +1,4 @@
 angular.module('ownApp',['my_services','my_filters'])
-    .config(function($httpProvider){
-        $httpProvider.defaults.cache=$cacheFactory('myCache',{capacity:20})//缓存前20个请求
-    })
     .controller('MainCtrl', ['$scope', 'UserInfo', function ($scope, UserInfo) { // 引用我们定义的UserInfo服务
         var promise = UserInfo.query(); // 同步调用，获得承诺接口
         promise.then(function(data) {  // 调用承诺API获取数据 .resolve
--- http/services.js
+++ http/services.js
@@ -5,12 +5,12 @@ angular.module('my_services',[])
     .factory('UserInfo', ['$http', '$q',"$cacheFactory", function ($http, $q,$cacheFactory) {
         return {
             query : function() {
-
+                var lruCache = $cacheFactory('lruCache',{ capacity : 10 });//自定义http缓存,但是我还没有完全弄懂
                 var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
                 $http({
                     method: 'GET',
                     url: './douban.php',
-                    cache: myCache
+                    cache: lruCache
                 }).success(function(data, status, headers, config) {
                     deferred.resolve(data);  // 声明执行成功，即http请求数据成功，可以返回数据了
                 }).
